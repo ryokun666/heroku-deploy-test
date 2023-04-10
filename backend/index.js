8562f374d94ee0e5b698f0a74362e12d2cb2599b
gulliver-r-morie <119906452+gulliver-r-morie@users.noreply.github.com>
2023-04-10 13:05:50 +0900

    api　modify

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("/", (req, res) => {
   res.send("Hello World!うぇーいa！!");
 });
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "はろーわーるど!" });
 });
 
 app.listen(port, () => {
