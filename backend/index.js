9b5747b81841e42eb0d30a7ed5c10f55cd6ed729
gulliver-r-morie <119906452+gulliver-r-morie@users.noreply.github.com>
2023-04-10 13:12:10 +0900

    gitignore追加

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 3001;
 
-
-app.get("/", (req, res) => {
-  res.send("Hello World!うぇーいa！!");
-});
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("/api", (req, res) => {
-  res.json({ message: "はろーわーるど!" });
+  res.json({ message: "Hello World!" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 app.listen(port, () => {
