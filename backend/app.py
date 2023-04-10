f52b47f78447db1a63ab8b2fa2015559e199890d
gulliver-r-morie <119906452+gulliver-r-morie@users.noreply.github.com>
2023-04-10 17:02:26 +0900

    frontendをNext.jsに変更！

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,21 @@
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory, Response
+from flask_cors import CORS # 追加
 import os
+import json
 
-app = Flask(__name__, static_folder="../frontend/build")
+app = Flask(__name__, static_folder="../frontend/out")
+CORS(app) # 追加
+
+print("🚀サーバー起動！")
 
 port = os.environ.get("PORT") or 3001
 
 
+
 @app.route("/api")
 def api():
-    return jsonify({"message": "PythonファイルのAPI連携成功ダ！！！"})
+    message = {"message": "PythonファイルのAPI連携成功ダ！！！"}
+    return Response(json.dumps(message, ensure_ascii=False), content_type="application/json; charset=utf-8")
 
 
 @app.route("/", defaults={"path": ""})
@@ -18,4 +25,4 @@ def catch_all(path):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="127.0.0.1", port=port)
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,17 +1,16 @@
-import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch("/api")
+    fetch("http://127.0.0.1:3001/api")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
   return (
     <div className="App">
       <h1>フロントエンド</h1>
-      <div>ぱいそんバージョン！</div>
+      <div>ねくすとじぇいえすバージョン！</div>
       <p>{message}</p>
     </div>
   );
