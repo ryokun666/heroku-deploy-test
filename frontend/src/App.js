f4ef5e16ed753eb936c8686dfd34fe0c8e81d3fa
gulliver-r-morie <119906452+gulliver-r-morie@users.noreply.github.com>
2023-04-10 14:40:01 +0900

    modify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 function App() {
   const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch("http://localhost:3001/api")
+    fetch("/api")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
