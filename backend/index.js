9d990b4c69821b44bcd6fdf018c1bbd98d5dd057
gulliver-r-morie <119906452+gulliver-r-morie@users.noreply.github.com>
2023-04-12 15:52:00 +0900

    modify

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/out")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "APIとの連携に成功！" });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/out/index.html"));
 });
 
 app.listen(port, () => {
